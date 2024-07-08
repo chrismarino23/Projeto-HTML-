b3c396f55156aa73b5c8220d3bb014f977283dee
Crystopher Marino <chrismarino23@gmail.com>
2024-07-08 10:11:48 -0300

    first commit shopcart, incluindo js no e-commerce

--- Js/script.js
+++ Js/script.js
@@ -32,4 +32,31 @@ document.addEventListener("DOMContentLoaded", function () {
       tabElement.click();
     }
   }
+
+  //close cart's modal at products screen.
+  btn_fechar_modal_cart_x.addEventListener("click", () => {
+    $("#cartModal").modal("hide");
+  });
+  btn_fechar_modal_cart.addEventListener("click", () => {
+    $("#cartModal").modal("hide");
+  });
 });
+
+//Modal add to cart
+
+function abrirModal(img, produto) {
+  let imagem = document.getElementById("imgSelecionada");
+  imagem.src = img;
+  imagem.style.width = "200px";
+  imagem.style.height = "200px";
+  document.getElementById("produtoSelecionado").innerText = `${produto}`;
+  document.getElementById("quantidade").value = 1;
+  $("#cartModal").modal("show");
+}
+
+function alterarQuantidade(delta) {
+  let quantidadeInput = document.getElementById("quantidade");
+  let quantidade = parseInt(quantidadeInput.value);
+  quantidade = Math.max(1, quantidade + delta); // Garante que a quantidade mínima é 1
+  quantidadeInput.value = quantidade;
+}
