35fb0f5fd72659a56048e69aba4ff6d606bbfa3e
Crystopher Marino <chrismarino23@gmail.com>
2024-07-17 23:41:54 -0300

    inclusão de adição no carrinho de compras

--- Js/script.js
+++ Js/script.js
@@ -49,20 +49,19 @@ document.addEventListener("DOMContentLoaded", function () {
 //Modal add to cart
 
 function abrirModal(img, produto, price) {
+  parseFloat(price);
   let imagem = document.getElementById("imgSelecionada");
   imagem.src = img;
   imagem.style.width = "200px";
   imagem.style.height = "200px";
   document.getElementById("produtoSelecionado").innerText = `${produto}`;
   document.getElementById("quantidade").value = 1;
-  document.getElementById("valuePrice").innerText = parseFloat(`${price}`)
-    .toFixed(2)
-    .toLocaleString("pt-BR", {
-      style: "currency",
-      currency: "BRL",
-    });
-
+  document.getElementById("valuePrice").innerText = parseFloat(
+    `${price}`
+  ).toFixed(2);
   $("#cartModal").modal("show");
+  document.getElementById("newPrice").style.display = "none";
+  document.getElementById("valuePrice").style.display = "";
 }
 
 function alterarQuantidade(delta) {
@@ -70,38 +69,143 @@ function alterarQuantidade(delta) {
   let quantidade = parseInt(quantidadeInput.value);
   quantidade = Math.max(1, quantidade + delta); // Garante que a quantidade mínima é 1
   quantidadeInput.value = quantidade;
+
   let price = document.getElementById("valuePrice").textContent;
   document.getElementById("valuePrice").style.display = "none";
 
   let newPrice = parseFloat(price * quantidade).toFixed(2);
-  console.log(newPrice);
-  document.getElementById("newPrice").innerText = `${newPrice}`;
-  document.getElementById("newPrice").style.display = "";
+  // console.log(newPrice);
+  let newPriceFront = document.getElementById("newPrice");
+  newPriceFront.innerText = `${newPrice}`;
+  newPriceFront.style.display = "";
 }
 
 var carrinho = [];
+var carrinhoObject = {
+  nome: "",
+  quantidade: "",
+  precoPKG: "",
+  precoFinal: "",
+};
 
 class ConstructCar {
   constructor(nameProduct, qtdKG, pricepKG, finalPrice) {
     this.nameProduct = nameProduct;
     this.qtdKG = parseInt(qtdKG);
-    this.pricepKG = parseFloat(pricepKG);
-    this.finalPrice = parseFloat(finalPrice);
+    this.pricepKG = parseFloat(pricepKG).toFixed(2);
+    this.finalPrice = parseFloat(finalPrice).toFixed(2);
   }
 }
 
-function adicionarAoCarrinho(nameProduct, qtdKG, pricepKG) {
-  let nameFront = document.getElementById("ninhoCMorango").textContent;
-  // let priceFront = document.getElementById("ninhoCMorango").textContent;
-  // let nameFront = document.getElementById("ninhoCMorango").textContent;
-  console.log(nameFront);
-  let finalPrice = qtdKG * pricepKG;
-  const newcart = new ConstructCar(nameProduct, qtdKG, pricepKG, finalPrice);
+document.getElementById("addToCart").addEventListener("click", () => {
+  let quantidadeInput = document.getElementById("quantidade").value;
+  // console.log(quantidadeInput);
 
-  carrinho.push[newcart];
+  let produtoSelecionado =
+    document.getElementById("produtoSelecionado").textContent;
+  // console.log(`produtoSelecionado = ` + produtoSelecionado);
 
-  // console.log(carrinho);
-  for (const desc in newcart) {
-    console.log(`${desc} = ${newcart[desc]}`);
+  let finalPrice = parseFloat(document.getElementById("newPrice").textContent);
+
+  if (isNaN(finalPrice)) {
+    let oldPrice = parseFloat(
+      document.getElementById("valuePrice").textContent
+    );
+    finalPrice = oldPrice;
   }
-}
+  // console.log(produtoSelecionado + finalPrice);
+
+  const cartItem = document.createElement("span");
+  cartItem.className = "list-group-item";
+  cartItem.style.display = "block";
+  cartItem.style.color = "saddlebrown";
+  cartItem.style.fontWeight = "bold";
+  cartItem.style.borderTop = "1px solid brown";
+  // cartItem.textContent = `${produtoSelecionado} - R$: ${finalPrice}`;
+
+  let pricepKG = finalPrice / quantidadeInput;
+  // console.log(pricepKG);
+  document.getElementById("cart").appendChild(cartItem);
+
+  const paragrafo = document.createElement("p"); // Cria um novo elemento <p>
+  const paragrafo2 = document.createElement("p"); // Cria um novo elemento <p>
+  const paragrafo3 = document.createElement("p"); // Cria um novo elemento <p>
+  const paragrafo4 = document.createElement("p"); // Cria um novo elemento <p>
+  const finalTotalPrice = document.createElement("p");
+
+  paragrafo.className = "list-group-item";
+  paragrafo2.className = "list-group-item";
+  paragrafo3.className = "list-group-item";
+  paragrafo4.className = "list-group-item";
+  finalTotalPrice.className = "list-group";
+
+  paragrafo.style.fontSize = "small";
+  paragrafo2.style.fontSize = "small";
+  paragrafo3.style.fontSize = "small";
+  paragrafo4.style.fontSize = "small";
+  finalTotalPrice.style.textAlign = "end";
+
+  //constructor of car
+  const newcartItem = new ConstructCar(
+    produtoSelecionado,
+    quantidadeInput,
+    pricepKG,
+    finalPrice
+  );
+  carrinho.push(newcartItem);
+
+  let precoFinalAtual = parseFloat(0);
+
+  if (!carrinho.lastChild) {
+    carrinho.forEach((elemento, index) => {
+      cartItem.textContent = `Item ${index + 1}: `;
+
+      paragrafo.textContent = elemento.nameProduct; // Define o texto do parágrafo
+      document.getElementById("cart").appendChild(paragrafo); // Adiciona o parágrafo ao container
+
+      paragrafo2.textContent = `Quantidade: ${elemento.qtdKG}`; // Define o texto do parágrafo
+      document.getElementById("cart").appendChild(paragrafo2); // Adiciona o parágrafo ao container
+
+      paragrafo3.textContent = `Preço por KG: R$ ${elemento.pricepKG}`; // Define o texto do parágrafo
+      document.getElementById("cart").appendChild(paragrafo3); // Adiciona o parágrafo ao container
+
+      paragrafo4.textContent = `Preço Total do Item: R$ ${elemento.finalPrice}`; // Define o texto do parágrafo
+      document.getElementById("cart").appendChild(paragrafo4); // Adiciona o parágrafo ao container
+
+      precoFinalAtual += parseFloat(elemento.finalPrice);
+      let remover = document.getElementById("idFinalTotalPrice");
+      if (remover) {
+        remover.remove();
+      }
+    });
+    console.log(`precoFinalAtual = ${precoFinalAtual}`);
+
+    if (finalTotalPrice !== 0) {
+      finalTotalPrice.textContent = `Valor total do Pedido: R$ ${precoFinalAtual}`;
+      finalTotalPrice.id = "idFinalTotalPrice";
+      document.getElementById("cart").appendChild(finalTotalPrice); // Adiciona o parágrafo ao container
+    }
+
+    // for (let i = 0; i < carrinho.length; i++) {
+    //   console.log(JSON.stringify(carrinho, null, 2));
+    // cartItem.textContent = JSON.stringify(carrinho[i], null, 2);
+    // cartItem.textContent = carrinho[i];
+
+    // cartItem.textContent = `${carrinho[i].nameProduct}\n
+    // Quantidade: ${carrinho[i].qtdKG}\n
+    // Preço por KG: R$ ${carrinho[i].pricepKG}\n
+    // Valor total: R$ ${carrinho[i].finalPrice}`;
+    // }
+    carrinho.forEach((produto, index) => {
+      console.log(`Item: ${index + 1}`);
+      console.log(`Produto: ${produto.nameProduct}`);
+      console.log(`Quantidade: ${produto.qtdKG}KG`);
+      console.log(`Preço por KG: R$ ${produto.pricepKG}`);
+      console.log(`Preço Final: R$ ${produto.finalPrice}`);
+    });
+  } else {
+    console.log("não leu o array de obj");
+  }
+
+  $("#cartModal").modal("hide");
+});
